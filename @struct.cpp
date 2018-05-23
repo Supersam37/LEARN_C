71e919aa84529224822d461de1ab85fdad89ca03
Supersam37 <31650641+Supersam37@users.noreply.github.com>
2018-05-23 16:07:04 +0800

    comments

--- @struct.cpp
+++ @struct.cpp
@@ -3,10 +3,10 @@
 struct student
 {
 	int num;
-	char name[20];
+	char name[20];//定义结构数组 
 };
 
-struct student stu[3] = {
+struct student stu[3] = {//声明结构数组 
 {10101,"lilin"},
 {10102,"Echo"},
 {10103,"Wiliam"}};
@@ -15,6 +15,6 @@ main(){
 	int i = 0;
 	struct student *p;
 	printf("NO\tNAME\n");
-	for(p=stu;p<stu+3;p++)
+	for(p=stu;p<stu+3;p++)//使用指针在结构数组中进行循环 
 	printf("%d\t%s\n",p->num,p->name);
 }
